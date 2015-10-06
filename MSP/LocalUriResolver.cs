aa72cc877061d4cfbed81eecb2ce236c2e443d64
aryamadhu <aryamadhu31@gmail.com>
2015-10-06 21:40:24 +0530

    MSP  6/10/2015
    Msp Application  Final version

---
+++ MSP/LocalUriResolver.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using Windows.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage.Streams;
+using Windows.Foundation;
+using Windows.Web;
+using Windows.Security.Cryptography;
+
+
+namespace MSP
+{
+    class LocalUriResolver : IUriToStreamResolver
+    {
+        public IAsyncOperation<IInputStream> UriToStreamAsync(Uri uri)
+        {
+            if (uri == null)
+            {
+                throw new Exception();
+            }
+            string path = uri.AbsolutePath;
+            return GetContent(path).AsAsyncOperation();
+        }
+
+        private async Task<IInputStream> GetContent(string uriPath)
+        {
+            try
+            {
+                Uri localUri = new Uri("ms-appx://" + uriPath);
+                StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(localUri);
+                IRandomAccessStream stream = await file.OpenReadAsync();
+                return stream.GetInputStreamAt(0);
+            }
+            catch (Exception)
+            {
+                throw new Exception("Invalid path");
+            }
+        }
+    }
+}
